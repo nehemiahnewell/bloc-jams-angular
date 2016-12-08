3847e9a8f36697d6234357b17e8ed261b2719087
nehemiahnewell <nehemiah.newell@gmail.com>
2016-12-08 19:53:46 +0000

    displays

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,4 +1,22 @@
 /*global angular*/
-
-angular.module('blocJams', []);
-
+(function() {
+  function config($stateProvider, $locationProvider) {
+    $locationProvider
+      .html5Mode({
+        enabled: true,
+        requireBase: false
+    });
+    $stateProvider
+      .state('landing', {
+      url: '/',
+      templateUrl: '/templates/landing.html'
+      })
+      .state('album', {
+        url: '/album',
+        templateUrl: '/templates/album.html'
+    });
+  }
+  angular
+    .module('blocJams', ['ui.router'])
+    .config(config);
+})();
